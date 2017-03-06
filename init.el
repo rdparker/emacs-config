b5450c858ad4a6f12faa97856dbcf77a37511069
Ron Parker <rdparker@gmail.com>
2017-03-06 18:12:50 -0500

    Add ELPA bits for Emacs 25.1.1
    One interesting bit is that with Emacs 25 package now expects to find a
    package-initialize call in your initialization file, but it doesn't care
    if you have it commented out or not.

--- init.el
+++ init.el
@@ -6,6 +6,13 @@
 
 ;;; Initialization
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
 (setq message-log-max 16384)
 
 (defconst emacs-start-time (current-time))
