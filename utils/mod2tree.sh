04554bfa8deb4be0bd7e6c6a7293df58492286bd
Ron Parker <rdparker@gmail.com>
2017-03-23 17:24:15 -0400

    Improve error handling and submodule HEAD detection in mod2tree.sh

--- utils/mod2tree.sh
+++ utils/mod2tree.sh
@@ -16,6 +16,19 @@ BEGIN{found=0}
 
 toplevel=$(pwd)
 
+function die ()
+{
+	die_with_status 1 "$@"
+}
+
+function die_with_status ()
+{
+	status=$1
+	shift
+	printf >&2 '%s\n' "$*"
+	exit "$status"
+}
+
 # This is similar to git subtree, but it cheats and fetches the entire
 # repository so that a raw commit id may be added as a subtree. The
 # git subtree command only supports branches and tags unless the
@@ -26,55 +39,55 @@ function subtree_add()
     mod=$2
     ref=$3
 
-    git fetch $mod
-    git read-tree --prefix=$subdir $ref
+    git fetch $mod || die "Cannot fetch $mod"
+    git read-tree --prefix=$subdir $ref || die "Cannot place $ref into $subdir"
     git checkout -- $subdir
 
-    child=$(git write-tree)
-    head=$(git rev-parse HEAD)
-    tree=$(git log -1 --pretty=format:%T $ref)
-    short=$(git rev-parse --short $tree)
+    child=$(git write-tree) || exit $?
+    head=$(git rev-parse HEAD) || exit $?
+    tree=$(git log -1 --pretty=format:%T $ref) || exit $?
+    short=$(git rev-parse --short $tree) || exit $?
     squash=$(git commit-tree $tree <<EOF
 Squashed '$subdir/' content from commit $short
 
 git-subtree-dir: $subdir
 git-subtree-split: $ref
 EOF
-	   )
+	   ) || exit $?
 
     merge=$(git commit-tree $child -p $head -p $squash <<EOF
 Merge commit '$squash' as '$subdir'
 EOF
-	 )
+	 ) || exit $?
 
-    git reset $merge || (echo git reset $merge failed >&2 && exit 1)
+    git reset $merge || die "Cannot reset to $merge"
 }
 
 function process_submodule()
 {
     local subdir=$1
 
-    local orig_branch=$(git rev-parse --abbrev-ref HEAD)
+    local orig_branch=$(git rev-parse --abbrev-ref HEAD) || exit $?
 
     # Get info about the submodule
+    local ref=$(cat .git/modules/$subdir/HEAD)
     cd $subdir
-    local origin=$(git remote -v | awk 'NR==1{print $2}')
-    local ref=$(git show-ref HEAD |awk '{print $1}')
-    local mod=$(basename $origin .git)
+    local origin=$(git remote -v | awk 'NR==1{print $2}') || exit $?
+    local mod=$(basename $origin .git) || exit $?
 
     ###
     ### Remove the submodule
     ###
-    cd -
+    cd - >/dev/null
 
     # Do all the work for a module and tree on a branch
-    git checkout -B update-$mod
+    git checkout -B update-$mod || die "Cannot checkout update-$mod"
 
     # Deinitialize the submodule
-    git submodule deinit $subdir
+    git submodule deinit $subdir || die "Cannot deinit $subdir"
 
     # Remove it from git's record of things
-    git rm -r --cached $subdir
+    git rm -r --cached $subdir || die "Cannot remove cached $subdir"
 
     # Remove the content
     rm -rf $subdir
@@ -82,31 +95,39 @@ function process_submodule()
     # Remove the module cache
     rm -rf .git/modules/$subdir
 
-    # Remove the entry from .gitmodules
-    sed -i "/$(echo $subdir|sed 's=/=\\/=g')\"[]]\$/,+2d" .gitmodules
+    # Remove the entry from any .gitmodules which reference it.
+    # sed -i "/$(echo $subdir|sed 's=/=\\/=g')\"[]]\$/,+2d" .gitmodules
+    for modfile in $(find -name .gitmodules); do
+	modfiledir=$(dirname $modfile | sed 's=^\./==')
+	relpath=$(echo $subdir | sed s=^$modfiledir/==)
+	sed -i "/$(echo $relpath|sed 's=/=\\/=g')\"[]]\$/,+2d" $modfile
+    done
 
     # If .gitmodules is empty, git remove it. Otherwise git add it
     if [ -s .gitmodules ]; then
-	git add .gitmodules
+	git add .gitmodules || die "Cannot add .gitmodules"
     else
-	git rm .gitmodules
+	git rm .gitmodules || die "Cannot remove .gitmodules"
     fi
 
     # Commit the changes
-    git commit -m "Remove the $mod submodule"
+    git commit -m "Remove the $mod submodule" || die "Error committing changes"
 
     ###
     ### Add a remote for the subtree
     ###
     awk -v mod=$mod -v origin=$origin "$awkscript" Makefile > Makefile.$$
     mv -f Makefile.$$ Makefile
-    git add Makefile
+    git add Makefile || die "Error adding Makefile"
     if ( echo $mod | grep -q '^[aeiou]'); then
-	git commit -m "Add a remote to support an $mod subtree"
+	git commit -m "Add a remote to support an $mod subtree" \
+	     || die "Error committing changes"
     else
-	git commit -m "Add a remote to support a $mod subtree"
+	git commit -m "Add a remote to support a $mod subtree" \
+	     || die "Error committing changes"
     fi
-    git remote add $mod $origin || ( echo If you restarted mod2tree please remove the $mod remote and try again. >&2 && exit 1)
+    git remote add $mod $origin || \
+	die "If you restarted mod2tree please remove the $mod remote and try again."
 
     ###
     ### Create the subtree
@@ -116,13 +137,13 @@ function process_submodule()
     ###
     ### Merge the update branch back into master and remove the branch
     ###
-    git checkout $orig_branch
-    git merge --no-edit --no-ff update-$mod
-    git branch -d update-$mod
+    git checkout $orig_branch || die "Cannot checkout $orig_branch"
+    git merge --no-edit --no-ff update-$mod || die "Cannot merge update-$mod"
+    git branch -d update-$mod || die "Cannot delete update-$mod"
     # There are times like with merging in yasnippet where this will
     # leave you with a dirty checkout. So do a reset so that only
     # submodules which are truly missing are hit in the next section.
-    git reset --hard
+    git reset --hard || exit $?
 
     ###
     ### Restore any missing nested submodules that may have been
@@ -135,43 +156,51 @@ function process_submodule()
 	    git status >&2
 	    exit 1
 	fi
-	git submodule update --init "$dir"
+	git submodule update --init "$dir" || \
+	    die "Error updating submodule $dir"
     done
 }
 
 function add_nested_submodules()
 {
     for x in $(find -name .gitmodules | sed 1d); do
-	dir=$(dirname $x | sed 's=^\./==')
-	escaped_dir=$(echo $dir| sed s=/=\\\\/=g)
-	paths=$(awk '/path = /{print $3}' $x | sed s/^/$escaped_dir\\//)
+	dir=$(dirname $x | sed 's=^\./==') || exit $?
+	escaped_dir=$(echo $dir| sed s=/=\\\\/=g) || exit $?
+	paths=$(awk '/path = /{print $3}' $x | sed s/^/$escaped_dir\\//) || \
+	    exit $?
+
 	for path in $paths; do
 	    if ! grep -q "path = $path" .gitmodules; then
 		cat $x | sed s=submodule\ \"=\&$escaped_dir/= | \
-		    sed s/path\ =\ /\&$escaped_dir\\// | tee -a .gitmodules
+		    sed s/path\ =\ /\&$escaped_dir\\// | tee -a .gitmodules || \
+		    exit $?
 	    fi
 	done
     done
 
     git status -s
     if [ $(git status -s | wc -l) -ne 0 ]; then
-	git add .gitmodules
-	git commit -m 'Add nested submodules at top level'
+	git add .gitmodules || die "Error adding .gitmodules"
+	git commit -m 'Add nested submodules at top level' || \
+	    die "Error committing changes"
     fi
 
     make submodules
 }
 
 
-initial_branch=$(git rev-parse --abbrev-ref HEAD)
-git checkout -b submodules-to-subtrees
+initial_branch=$(git rev-parse --abbrev-ref HEAD) || exit $?
+git checkout -b submodules-to-subtrees || exit $?
 
 add_nested_submodules
 
 for submod in $(git submodule | awk '{print $2}'); do
     process_submodule $submod
+    echo "If Emacs does not automatically exit, eval '(kill-emacs 1)'."
+    emacs --debug-init -e kill-emacs || \
+	die "Submodule $submod broke Emacs cleanly exiting."
 done
 
-git checkout "$initial_branch"
-git merge --no-ff submodules-to-subtrees
+git checkout "$initial_branch" || exit $?
+git merge --no-edit --no-ff submodules-to-subtrees || exit $?
 git branch -d submodules-to-subtrees
