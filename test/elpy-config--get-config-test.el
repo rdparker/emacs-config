1eb9676f41b820106ac12d5aa24ef5c46e02d61c
Ron Parker <rdparker@gmail.com>
2017-03-24 14:11:36 -0400

    Squashed 'site-lisp/elpy/' content from commit b927bf5d9
    git-subtree-dir: site-lisp/elpy
    git-subtree-split: 8e307d127858b5b65fb6af066d959451578e590d

---
+++ test/elpy-config--get-config-test.el
@@ -0,0 +1,26 @@
+(ert-deftest elpy-config--get-config-should-return-keys ()
+  (elpy-testcase ()
+    (let ((config (elpy-config--get-config)))
+      (dolist (key '("emacs_version"
+                     "python_rpc"
+                     "python_rpc_executable"
+                     "python_interactive"
+                     "python_interactive_executable"
+                     "python_version"
+                     "elpy_version"
+                     "jedi_version"
+                     "rope_version"
+                     "virtual_env"
+                     "virtual_env_short"))
+        (should (not (eq :not-set (gethash key config :not-set))))))))
+
+(ert-deftest elpy-config--get-config-should-set-pythonpath ()
+  (elpy-testcase ()
+    (mletf* ((elpy-rpc--environment () "test-environment")
+             (environment nil)
+             (call-process (&rest ignored)
+                           (setq environment process-environment)))
+
+      (elpy-config--get-config)
+
+      (should (equal environment "test-environment")))))
