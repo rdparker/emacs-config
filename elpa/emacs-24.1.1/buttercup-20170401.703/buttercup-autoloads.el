8bcb55b240d0532f17a6cd7de8311472d502ddfe
Ron Parker <rdparker@gmail.com>
2017-04-06 12:34:39 -0400

    Add ELPA packages for several versions of Emacs

---
+++ elpa/emacs-24.1.1/buttercup-20170401.703/buttercup-autoloads.el
@@ -0,0 +1,43 @@
+;;; buttercup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (buttercup-run-markdown buttercup-run-discover
+;;;;;;  buttercup-run-at-point) "buttercup" "buttercup.el" (22758
+;;;;;;  27722))
+;;; Generated autoloads from buttercup.el
+
+(autoload 'buttercup-run-at-point "buttercup" "\
+Run the buttercup suite at point.
+
+\(fn)" t nil)
+
+(autoload 'buttercup-run-discover "buttercup" "\
+Discover and load test files, then run all defined suites.
+
+Takes directories as command line arguments, defaulting to the
+current directory.
+
+\(fn)" nil nil)
+
+(autoload 'buttercup-run-markdown "buttercup" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("buttercup-compat.el" "buttercup-pkg.el")
+;;;;;;  (22758 27723 8135))
+
+;;;***
+
+(provide 'buttercup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; buttercup-autoloads.el ends here
