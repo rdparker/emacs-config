0c3260b411a4bf3cac4b37c9fc47e83f2b747c57
Ron Parker <rdparker@gmail.com>
2017-04-14 13:13:27 -0400

    Add Mike Zamansky's sample hydra for Elfeed
    This will be replaced with either macros or my own hydra later.

--- init.el
+++ init.el
@@ -697,7 +697,9 @@ it."
 	 ("q" . bjm/elfeed-save-db-and-bury)
 	 ("Q" . bjm/elfeed-save-db-and-bury)
 	 ("m" . elfeed-toggle-star)
-	 ("M" . elfeed-toggle-star))
+	 ("M" . elfeed-toggle-star)
+	 ("j" . mz/hydra-elfeed/body)
+	 ("J" . mz/hydra-elfeed/body))
   :commands elfeed-db-load
   :init
   (defun elfeed-mark-all-as-read ()
@@ -749,7 +751,21 @@ the search buffer is entered."
     :load-path "site-lisp/elfeed-org"
     :config
     (elfeed-org)
-    (setq rmh-elfeed-org-files '("~/Dropbox/Emacs/elfeed.org"))))
+    (setq rmh-elfeed-org-files '("~/Dropbox/Emacs/elfeed.org")))
+
+  (use-package hydra :load-path "site-lisp/hydra")
+
+  (defhydra mz/hydra-elfeed ()
+   "filter"
+   ("c" (elfeed-search-set-filter "@6-months-ago +cs") "cs")
+   ("e" (elfeed-search-set-filter "@6-months-ago +emacs") "emacs")
+   ("d" (elfeed-search-set-filter "@6-months-ago +education") "education")
+   ("*" (elfeed-search-set-filter "@6-months-ago +star") "Starred")
+   ("M" elfeed-toggle-star "Mark")
+   ("A" (elfeed-search-set-filter "@6-months-ago") "All")
+   ("T" (elfeed-search-set-filter "@1-day-ago") "Today")
+   ("Q" bjm/elfeed-save-db-and-bury "Quit Elfeed" :color blue)
+   ("q" nil "quit" :color blue)))
 
 ;;; elide-head -- elide most of standard license header text
 (use-package elide-head
