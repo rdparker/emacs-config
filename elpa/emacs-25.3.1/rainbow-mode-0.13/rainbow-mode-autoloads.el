d955ef5ce0201594612d6182ee301b3dd4d5c678
Ron Parker <rdparker@gmail.com>
2018-02-21 14:47:00 -0500

    Add ELPA files for Emacs 25.3.1

---
+++ elpa/emacs-25.3.1/rainbow-mode-0.13/rainbow-mode-autoloads.el
@@ -0,0 +1,15 @@
+;;; rainbow-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("rainbow-mode.el") (23181 51878 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; rainbow-mode-autoloads.el ends here
