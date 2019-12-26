761438c2bf623722e9d0f3f783fa1fba458b2a6a
Ron Parker <rdparker@gmail.com>
2019-12-26 19:46:31 +0000

    Setup magit

--- init.el
+++ init.el
@@ -9,6 +9,10 @@
 (global-set-key (kbd "M-SPC M-SPC") 'set-mark-command)
 (global-set-key (kbd "M-SPC SPC") 'just-one-space)
 
+(package-initialize)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
 (column-number-mode 1)
 
 (setq site-lisp-dir (expand-file-name "site-lisp" user-emacs-directory))
@@ -21,3 +25,17 @@
   (setq icicle-Completions-text-scale-decrease 0))
 
 (require 'use-package)
+(use-package magit :ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
