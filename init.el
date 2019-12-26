140b0577f709fdf5f2c458a6287449e26cb55ab7
Ron Parker <rdparker@gmail.com>
2019-12-26 17:52:05 +0000

    Initial commit
    Icicles plus a very basic Emacs init file.

---
+++ init.el
@@ -0,0 +1,20 @@
+;; Rebind set-mark for Termius.
+;;
+;; Since just-one-space is normally bound to M-SPC, rebind it too.
+;;
+;; My logic here is that I use set-mark much more so not having to
+;; release Meta is faster.
+(define-prefix-command 'meta-space-map)
+(global-set-key (kbd "M-SPC") 'meta-space-map)
+(global-set-key (kbd "M-SPC M-SPC") 'set-mark-command)
+(global-set-key (kbd "M-SPC SPC") 'just-one-space)
+
+
+(column-number-mode 1)
+
+(add-to-list 'load-path (expand-file-name "icicles" user-emacs-directory))
+
+(require 'icicles)
+(icy-mode 1)
+(when (not window-system)
+  (setq icicle-Completions-text-scale-decrease 0))
